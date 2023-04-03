f0822bdd3d5eb21f4f4252d55f77647bf19a673f
Yael Harosh <yaelharosh01@gmail.com>
2023-04-03 17:52:36 -0400

    not solved

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,10 @@
 def oxford_comma(items):
-    return None
+    str = ' '
+    for index, item in enumerate(items):
+        # print(index, items[index])
+        if index != len(items) - 1:
+            str += ','.join([item])
+        else:
+            str += ', and '.join([item])
+    return str
+print(oxford_comma(['kiwi', 'salad', 'banana']))
